0e8af58712cc445fa7aed10e0a65fc6068ccefe0
Zeidant <anthonyemma55@gmail.com>
2023-10-01 17:56:56 -0400

    Updating for music player
    Season 4 started

---
+++ js/music-list.js
@@ -0,0 +1,321 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+   {
+    "name": "Attention",
+    "artist": "NewJeans",
+    "img": "music-1",
+    "src": "music-1"
+  },
+  {
+    "name": "Love Shot",
+    "artist": "EXO",
+    "img": "music-2",
+    "src": "music-2"
+  },
+  {
+    "name": "SOLO",
+    "artist": "JENNIE",
+    "img": "music-3",
+    "src": "music-3"
+  },
+  {
+    "name": "dimple",
+    "artist": "BTS",
+    "img": "music-4",
+    "src": "music-4"
+  },
+  {
+    "name": "Golden Brown",
+    "artist": "The Stranglers",
+    "img": "music-5",
+    "src": "music-5"
+  },
+  {
+    "name": "Fast Car",
+    "artist": "Jonas Blue",
+    "img": "music-6",
+    "src": "music-6"
+  },
+  {
+    "name": "Die 4 Me",
+    "artist": "Halsey",
+    "img": "music-7",
+    "src": "music-7"
+  },
+  {
+    "name": "Small Worlds",
+    "artist": "Mac Miller",
+    "img": "music-8",
+    "src": "music-8"
+  },
+  {
+    "name": "Rosa Pastel",
+    "artist": "Belanova",
+    "img": "music-9",
+    "src": "music-9"
+  },
+  {
+    "name": "Mi Bendici\u00f3n",
+    "artist": "Juan Luis Guerra 4.40",
+    "img": "music-10",
+    "src": "music-10"
+  },
+  {
+    "name": "micha",
+    "artist": "Iliona",
+    "img": "music-11",
+    "src": "music-11"
+  },
+  {
+    "name": "Mi Luz (ft. Rels B)",
+    "artist": "Rvfv",
+    "img": "music-12",
+    "src": "music-12"
+  },
+  {
+    "name": "Snowflakes Fall",
+    "artist": "Nic D",
+    "img": "music-13",
+    "src": "music-13"
+  },
+  {
+    "name": "Saturday Nights REMIX",
+    "artist": "Khalid",
+    "img": "music-14",
+    "src": "music-14"
+  },
+  {
+    "name": "Kings and Queens of Wasteland",
+    "artist": "Jukio Kallio",
+    "img": "music-15",
+    "src": "music-15"
+  },
+  {
+    "name": "Crown",
+    "artist": "Kendrick Lamar",
+    "img": "music-16",
+    "src": "music-16"
+  },
+  {
+    "name": "Dodgeball",
+    "artist": "Pauline Herr",
+    "img": "music-17",
+    "src": "music-17"
+  },
+  {
+    "name": "Cocoon",
+    "artist": "070 Shake",
+    "img": "music-18",
+    "src": "music-18"
+  },
+  {
+    "name": "I GUESS IT'S LOVE?",
+    "artist": "The Kid LAROI",
+    "img": "music-19",
+    "src": "music-19"
+  },
+  {
+    "name": "YOSEMITE",
+    "artist": "Travis Scott",
+    "img": "music-20",
+    "src": "music-20"
+  },
+  {
+    "name": "Stolen Dance",
+    "artist": "Milky Chance",
+    "img": "music-21",
+    "src": "music-21"
+  },
+  {
+    "name": "A Pal\u00e9",
+    "artist": "ROSAL\u00cdA",
+    "img": "music-22",
+    "src": "music-22"
+  },
+  {
+    "name": "Move Your Feet",
+    "artist": "Junior Senior",
+    "img": "music-23",
+    "src": "music-23"
+  },
+  {
+    "name": "QQTF",
+    "artist": "Lena Dardelet",
+    "img": "music-24",
+    "src": "music-24"
+  },
+  {
+    "name": "Las Divinas",
+    "artist": "Patito Feo",
+    "img": "music-25",
+    "src": "music-25"
+  },
+{
+    "name": "After Hours",
+    "artist": "The Weeknd",
+    "img": "music-26",
+    "src": "music-26"
+  },
+  {
+    "name": "I Need U",
+    "artist": "BTS",
+    "img": "music-27",
+    "src": "music-27"
+  },
+  {
+    "name": "Why Can't You Wait",
+    "artist": "The Chainsmokers",
+    "img": "music-28",
+    "src": "music-28"
+  },
+  {
+    "name": "About Damn Time",
+    "artist": "Lizzo",
+    "img": "music-29",
+    "src": "music-29"
+  },
+  {
+    "name": "Wild Side (feat. Cardi B)",
+    "artist": "Normani",
+    "img": "music-30",
+    "src": "music-30"
+  },
+  {
+    "name": "Boys Don't Cry",
+    "artist": "The Cure",
+    "img": "music-31",
+    "src": "music-31"
+  },
+  {
+    "name": "Good Ones",
+    "artist": "Charli XCX",
+    "img": "music-32",
+    "src": "music-32"
+  },
+  {
+    "name": "AL BORDE",
+    "artist": "Milo j",
+    "img": "music-33",
+    "src": "music-33"
+  },
+  {
+    "name": "Backstabber",
+    "artist": "Kesha",
+    "img": "music-34",
+    "src": "music-34"
+  },
+  {
+    "name": "Suelta",
+    "artist": "Jay Wheeler",
+    "img": "music-35",
+    "src": "music-35"
+  },
+  {
+    "name": "Set Me Free Pt.2",
+    "artist": "Jimin",
+    "img": "music-36",
+    "src": "music-36"
+  },
+  {
+    "name": "Piece Of Your Heart",
+    "artist": "MEDUZA",
+    "img": "music-37",
+    "src": "music-37"
+  },
+  {
+    "name": "DE CAROLINA",
+    "artist": "Rauw Alejandro",
+    "img": "music-38",
+    "src": "music-38"
+  },
+  {
+    "name": "Bandaid",
+    "artist": "Tawnted",
+    "img": "music-39",
+    "src": "music-39"
+  },
+  {
+    "name": "CHARGER",
+    "artist": "ELIO",
+    "img": "music-40",
+    "src": "music-40"
+  },
+  {
+    "name": "Go!",
+    "artist": "NEFFEX",
+    "img": "music-41",
+    "src": "music-41"
+  },
+  {
+    "name": "Bad Dream Baby",
+    "artist": "Hippo Campus",
+    "img": "music-42",
+    "src": "music-42"
+  },
+  {
+    "name": "Equally Lost (feat. Doja Cat)",
+    "artist": "Tove Lo",
+    "img": "music-43",
+    "src": "music-43"
+  },
+  {
+    "name": "Sue\u00f1os Sobre Ti",
+    "artist": "Yellow Outlet",
+    "img": "music-44",
+    "src": "music-44"
+  },
+  {
+    "name": "Paseo",
+    "artist": "Estopa",
+    "img": "music-45",
+    "src": "music-45"
+  },
+  {
+    "name": "En Alta",
+    "artist": "J Balvin",
+    "img": "music-46",
+    "src": "music-46"
+  },
+  {
+    "name": "antes de perderte (OG VERSION)",
+    "artist": "Duki",
+    "img": "music-47",
+    "src": "music-47"
+  },
+  {
+    "name": "Am I Dreaming (Metro Boomin & A$AP Rocky, Roisee)",
+    "artist": "Metro Boomin",
+    "img": "music-48",
+    "src": "music-48"
+  },
+  {
+    "name": "si preguntas por mi",
+    "artist": "Tainy",
+    "img": "music-49",
+    "src": "music-49"
+  },
+  {
+    "name": "Como Yo :(",
+    "artist": "Marshmello",
+    "img": "music-50",
+    "src": "music-50"
+  },
+  
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
