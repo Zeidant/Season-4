333f2b87cd374b487b62ec0bf9ad2c2a99adba28
Zeidant <anthonyemma55@gmail.com>
2023-10-03 15:01:02 -0400

    Adding song day 3

--- js/music-list.js
+++ js/music-list.js
@@ -11,23 +11,22 @@
 
 let allMusic = [
    {
-    "name": "Attention",
+    "name": "1. Attention",
     "artist": "NewJeans",
     "img": "music-1",
     "src": "music-1"
   },
 
   {
-    "name": "Love Shot",
+    "name": "2. Love Shot",
     "artist": "Exo",
     "img": "music-2",
     "src": "music-2"
   },
-  // like this paste it and remember to give comma after ending of this bracket }
-  // {
-  //   name: "Here is the music name",
-  //   artist: "Here is the artist name",
-  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
-  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
-  // }
+  {
+    "name": "3. Solo
+    "artist": "Jennie",
+    "img": "music-3",
+    "src": "music-3"
+  }
 ];
